0922949e012ac1ff12049dca690161f63ca213d1
christian keusch <weitnow@gmail.com>
2022-06-21 21:09:33 +0200

    attack with collider added, character flips to face each other added

--- fighter.py
+++ fighter.py
@@ -1,35 +1,51 @@
 import pygame
 
+
 class Fighter():
     def __init__(self, x, y):
+        self.flip = False
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
         self.jump = False
+        self.attacking = False
+        self.attack_type = 0
+        self.health = 100
 
-    def move(self, screen_width, screen_height):
+    def move(self, screen_width, screen_height, surface, target):
         SPEED = 10
         GRAVITY = 2
         dx = 0
         dy = 0
 
-        #get keypresses
-        key = pygame.key.get_pressed()
+        # get keypresses
+        self.pressed = pygame.key.get_pressed()
+        key = self.pressed
 
-        #movement
-        if key[pygame.K_a]:
-            dx = -SPEED
-        if key[pygame.K_d]:
-            dx = SPEED
-        #jump
-        if key[pygame.K_w] and not self.jump:
-            self.vel_y = - 30
-            self.jump = True
+        # can only perform other actions if not currently attacking
+        if self.attacking == False:
+            # movement
+            if key[pygame.K_a]:
+                dx = -SPEED
+            if key[pygame.K_d]:
+                dx = SPEED
+            # jump
+            if key[pygame.K_w] and not self.jump:
+                self.vel_y = - 30
+                self.jump = True
+            # attack
+            if key[pygame.K_r] or key[pygame.K_t]:
+                self.attack(surface, target)
+                # determine which attack type was used
+                if key[pygame.K_r]:
+                    self.attack_type = 1
+                if key[pygame.K_t]:
+                    self.attack_type = 2
 
-        #apply gravity
+        # apply gravity
         self.vel_y += GRAVITY
         dy += self.vel_y
 
-        #ensure player stays on screen
+        # ensure player stays on screen
         if self.rect.left + dx < 0:
             dx = 0 - self.rect.left
         if self.rect.right + dx > screen_width:
@@ -39,12 +55,22 @@ class Fighter():
             self.jump = False
             dy = screen_height - 110 - self.rect.bottom
 
-        #update player position
+        # ensure players face each other
+        if target.rect.centerx > self.rect.centerx:
+            self.flip = False
+        else:
+            self.flip = True
+
+        # update player position
         self.rect.x += dx
         self.rect.y += dy
 
+    def attack(self, surface, target):
+        self.attacking = True
+        attacking_rect = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
+        if attacking_rect.colliderect(target.rect):
+            target.health -= 10
+        pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
 
     def draw(self, surface):
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
-
-
--- main.py
+++ main.py
@@ -14,14 +14,29 @@ pygame.display.set_caption('Gemetzel')
 clock = pygame.time.Clock()
 FPS = 60
 
+#define colors
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+
 # load background image
 bg_image = pygame.image.load('assets/images/background/background.jpg').convert_alpha()
 
+#load spritesheets
+warrior_sheet = pygame.image.load('assets/images/warrior/Sprites/warrior.png').convert_alpha()
+wizard_sheet = pygame.image.load('assets/images/wizard/Sprites/wizard.png').convert_alpha()
 
 # function for drawing background
 def draw_bg():
     scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
     screen.blit(scaled_bg, (0, 0))
+
+# function for drawing fighter health bars
+def draw_health_bar(health, x, y):
+    ratio = health / 100
+    pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
+    pygame.draw.rect(screen, RED, (x, y, 400, 30))
+    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
 
 #create two instances of fighters
 fighter_1 = Fighter(200, 310)
@@ -37,8 +52,12 @@ while run:
     #draw background
     draw_bg()
 
+    #show player stats
+    draw_health_bar(fighter_1.health, 20, 20)
+    draw_health_bar(fighter_2.health, 580, 20)
+
     #move fighters
-    fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT)
+    fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
 
     #draw the fighters
     fighter_1.draw(screen)
